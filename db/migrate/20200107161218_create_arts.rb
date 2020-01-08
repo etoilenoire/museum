6a29f4e05d3008f32eaf7dbc9e0e24279b5b6839
etoilenoire <pvelain@gmail.com>
2020-01-08 16:09:41 +0100

    init

---
+++ db/migrate/20200107161218_create_arts.rb
@@ -0,0 +1,13 @@
+class CreateArts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :arts do |t|
+      t.string :title
+      t.string :author
+      t.integer :date
+      t.text :description
+      t.string :photo
+
+      t.timestamps
+    end
+  end
+end
